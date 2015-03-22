8d81747c149c0f1376ac07dda1a79b98125975e9
Dmitriy Fedoseev <fedoseev.dmitriy@gmail.com>
2015-03-22 16:17:05 +0300

    First commit

---
+++ RolyPolySwift/CardListDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  CardListDataSource.swift
+//  RolyPolySwift
+//
+//  Created by Dmitry Fedoseev on 22.03.15.
+//  Copyright (c) 2015 Dmitry Fedoseev. All rights reserved.
+//
+
+import Foundation
---
+++ RolyPolySwift/RolyPolySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RolyPolySwift
+//
+//  Created by Dmitry Fedoseev on 22.03.15.
+//  Copyright (c) 2015 Dmitry Fedoseev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
